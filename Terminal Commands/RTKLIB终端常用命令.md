6de44961b7e61dd9e366e05135dc0d98ea40ca7c
“xxhhhao” <2782473978@qq.com>
2025-08-07 01:18:54 +0800

    Upload RTKLIB

---
+++ Terminal Commands/RTKLIB终端常用命令.md
@@ -0,0 +1,9 @@
+# RTKLIB终端单点定位测试命令
+
+## 注意：将路径更换为自己的文件路径
+
+```c
+./rnx2rtkp.exe -x 5 -p 0 -m 15 -n -o E:\\rtklib_vscode\\result\\out.pos E:\\rtklib_vscode\\test_data\\rinex\\07590920.05o E:\\rtklib_vscode\\test_data\\rin
+ex\\07590920.05n
+```
+
---
+++ build/CMakeFiles/rnx2rtkp.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/rtklib_vscode/rtklib/src/convgpx.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/convgpx.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/convgpx.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/convkml.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/convkml.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/convkml.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/convrnx.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/convrnx.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/convrnx.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/datum.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/datum.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/datum.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/download.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/download.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/download.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/ephemeris.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ephemeris.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ephemeris.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/geoid.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/geoid.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/geoid.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/gis.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/gis.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/gis.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/ionex.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ionex.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ionex.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/lambda.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/lambda.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/lambda.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/options.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/options.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/options.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/pntpos.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/pntpos.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/pntpos.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/postpos.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/postpos.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/postpos.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/ppp.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ppp.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ppp.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/ppp_ar.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ppp_ar.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/ppp_ar.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/preceph.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/preceph.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/preceph.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/binex.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/binex.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/binex.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/crescent.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/crescent.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/crescent.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/javad.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/javad.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/javad.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/novatel.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/novatel.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/novatel.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/nvs.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/nvs.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/nvs.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/rt17.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/rt17.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/rt17.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/septentrio.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/septentrio.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/septentrio.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/skytraq.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/skytraq.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/skytraq.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/ss2.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/ss2.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/ss2.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcv/ublox.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/ublox.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcv/ublox.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rcvraw.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcvraw.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rcvraw.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rinex.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rinex.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rinex.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rnx2rtkp.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rnx2rtkp.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rnx2rtkp.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtcm.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtcm2.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm2.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm2.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtcm3.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm3.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm3.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtcm3e.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm3e.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtcm3e.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtkcmn.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtkcmn.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtkcmn.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtkpos.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtkpos.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtkpos.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/rtksvr.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtksvr.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/rtksvr.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/sbas.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/sbas.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/sbas.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/solution.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/solution.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/solution.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/stream.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/stream.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/stream.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/streamsvr.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/streamsvr.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/streamsvr.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/tides.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/tides.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/tides.c.obj.d"
+  "E:/rtklib_vscode/rtklib/src/tle.c" "CMakeFiles/rnx2rtkp.dir/rtklib/src/tle.c.obj" "gcc" "CMakeFiles/rnx2rtkp.dir/rtklib/src/tle.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
